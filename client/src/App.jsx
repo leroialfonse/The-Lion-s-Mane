cfa56bffe50feb36c5d10f14ca04550ac764ebb3
Brandon White <brandonwhitedev@gmail.com>
2024-08-01 22:40:41 -0400

    Working on adding content to pages, considering the routihg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
       <header>
         <h1>The Lion's Mane</h1>
         <nav>
-          <a href="/">Home</a> | <a href="/staff">Staff</a> | <a href="/services">Services</a> | <a href="booking">Booking</a> | <a href="admin">Admin</a>
+          <a href="/">Home</a> | <a href="/staff">Staff</a> | <a href="/services">Services</a> | <a href="/booking">Booking</a> | <a href="/admin">Admin</a>
         </nav>
       </header>
       <main>
--- client/src/pages/Booking.jsx
+++ client/src/pages/Booking.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+// Access to react calendar component. Easier than building my own.
+import Calendar from 'react-calendar'
+// Will allow for redirects to the homepage after a user completes making a booking.
+import { useNavigate } from 'react-router-dom'
+
+const services = [
+    'Retwist',
+    'Retwist w/Style',
+    'Detox',
+    'Color',
+    'Consultation'
+];
+
+const locticians = [
+    'BossLady',
+    'Newie',
+    'Jan Michael Vincent'
+]
 
 const Booking = () => {
     return (
-        <>
-            <h1>this is the Bookingpage!</h1>
-        </>
+        <div>
+            {/* A form for booking */}
+            <h2>Book your Appointment</h2>
+            {/* We want to control the form ourself. So, we're going to use a custom function and a prevent default, so that the page doesn't refresh onSubmit. We're stripping out the default function. So, we'll use some state here. */}
+            <form onSubmit={handleSubmit}>
+                <input id="name">Name: </input>
+                <input id="email">Email: </input>
+                <input id="service">Service: </input>
+                <input id="loctician">Loctician: </input>
+                <button type="submit">Book</button>
+            </form>
+        </div>
     )
 }
 
